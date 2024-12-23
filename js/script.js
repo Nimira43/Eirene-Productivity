85d53220b94e2540811c1fd6f5df055bad472d8a
Nimira43 <n.rabone72@aol.com>
2024-12-23 23:22:10 +0000

    Corrected some issues with script

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   toggleBtn.addEventListener('click', () => mobileMenu.classList.toggle('active'))
 })
 
-window.addEventListener('scroll', function() {
-  const navbar = this.document.querySelector('.navbar')
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar')
 
   if (window.scrollY > 0) {
     navbar.classList.add('navbar--scroll')
