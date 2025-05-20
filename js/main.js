53bbbe530f7c45ed7264d6b134358d46875582d8
Nimira43 <n.rabone72@aol.com>
2025-05-20 20:16:00 +0100

    Correcting code.

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@ function updateAll() {
   if (ballX < 0) ballSpeedX *= -1
   if (ballX > canvas.width) ballSpeedX *= -1
   if (ballY < 0) ballSpeedY *= -1
-  if (ballX > canvas.height) ballSpeedX *= -1
+  if (ballY > canvas.height) ballSpeedY *= -1
   
   ctx.fillStyle = 'black'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
   ctx.fillStyle = 'white'
   ctx.beginPath()
-  ctx.arc(ballX, 100, 10, 0, Math.PI * 2, true)
+  ctx.arc(ballX, ballY, 10, 0, Math.PI * 2, true)
   ctx.fill()
 }
