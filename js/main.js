759025074053fd2557bd0e92fff4f5d56821a4da
Nimira43 <n.rabone72@aol.com>
2025-05-20 20:48:41 +0100

    New drawAll function calling in colourRect and c0lourCircle to ball and canvas.

--- js/main.js
+++ js/main.js
@@ -27,12 +27,8 @@ function moveAll() {
 }
 
 function drawAll() {
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = 'white'
-  ctx.beginPath()
-  ctx.arc(ballX, ballY, 10, 0, Math.PI * 2, true)
-  ctx.fill()
+  colourRect(0, 0, canvas.width, canvas.height, '#000')
+  colourCircle(ballX, ballY, 10, '#fff')
 }
 
 function colourRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColour) {
