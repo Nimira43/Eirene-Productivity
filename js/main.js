f81bbdddd6141fd3fb8625c347a29b011cb8d015
Nimira43 <n.rabone72@aol.com>
2025-05-20 20:05:33 +0100

    Ball and fps

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+let ballX = 75
 let canvas, ctx
 
 window.onload = function () {
   canvas = document.getElementById('gameCanvas')
   ctx = canvas.getContext('2d')
+  fps = 30
+  setInterval(updateAll, 1000 / fps)
 }
+
+function updateAll() { }
