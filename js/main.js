2bab59a6d3afd3a0145e23d743216dc797a75fb8
Nimira43 <n.rabone72@aol.com>
2025-05-21 21:57:11 +0100

    Updated ballMove function

--- js/main.js
+++ js/main.js
@@ -97,9 +97,9 @@ function ballMove() {
   ballX += ballSpeedX
   ballY += ballSpeedY
 
-  if (ballX < 0) ballSpeedX *= -1
-  if (ballX > canvas.width) ballSpeedX *= -1
-  if (ballY < 0) ballSpeedY *= -1
+  if (ballX < 0 && ballSpeedX < 0.0) ballSpeedX *= -1
+  if (ballX > canvas.width && ballSpeedX > 0.0) ballSpeedX *= -1
+  if (ballY < 0 && ballSpeedY < 0.0) ballSpeedY *= -1
   if (ballY > canvas.height) {
     ballReset()
     brickReset()
